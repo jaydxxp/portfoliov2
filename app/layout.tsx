9248615dc322c8e979c7981e5a4209692cf2d7bc
jaydxxp <jaydeepwaghaskar2005@gmail.com>
2025-12-11 21:16:46 +0530

    added some metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -103,7 +103,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
+    apple: "/favicon.ico",
   },
 
 };
